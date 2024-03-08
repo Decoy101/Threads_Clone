0dafb290f51d1d68bc7eac96799ffe40d590af7b
Decoy101 <00amangpt00@gmail.com>
2024-03-09 00:12:46 +0530

    Initial Commit

---
+++ Threads_Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads_Clone
+//
+//  Created by Aman Gupta on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
